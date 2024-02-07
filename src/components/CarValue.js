2ba919be6aafde8089fb127f00f764e06cfa9a67
Laleh Shahidi <laleh.shahidi78@gmail.com>
2024-02-07 19:04:24 +0100

    added total cost logic

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,7 +1,23 @@
+// to figure out what cars are currently displayed on the screen, we need to access our state, we need to get access to a list of cars, 
+// we need to get access to whatever the current search term is, we need to do the same kind of filtering logic, but this time, instead 
+// of returning a list of all the cars, we need to take a look at the filtered list of cars, and sum up the cost of all of them.
+
+import { useSelector } from "react-redux";
+
 function CarValue() {
+    const totalCost = useSelector(({ cars: { data, searchTerm }} ) => {
+        const filteredCars = data.filter((car) => {
+            return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+        });
+        return filteredCars.reduce((acc, car) => {
+            return acc + car.cost
+        }, 0)
+    });
 
     return (
-        <div>CarValue</div>
+        <div className="car-value">
+            Total Cost: ${totalCost} 
+        </div>
     )
 };
 
